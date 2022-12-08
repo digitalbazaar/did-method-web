081e028826820e85b5fc6f777bea2a7465b49a68
Andrew Jones <ajones@digitalbazaar.com>
2022-12-08 12:18:38 -0500

    Add nock to tests.

--- test/driver.spec.js
+++ test/driver.spec.js
@@ -8,17 +8,21 @@ const {expect} = chai;
 import {driver} from '../lib/index.js';
 import {Ed25519VerificationKey2018} from
   '@digitalbazaar/ed25519-verification-key-2018';
+import nock from 'nock';
 
 const didWebDriver = driver();
 
 // eslint-disable-next-line max-len
 const TEST_SEED = '8c2114a150a16209c653817acc7f3e7e9c6c6290ae93d6689cbd61bb038cd31b';
 const TEST_DID = 'did:web:w3c-ccg.github.io:user:alice';
-const TEST_URL = 'https://w3c-ccg.github.io/user/alice';
+const host = 'https://w3c-ccg.github.io';
+const path = '/user/alice';
+const TEST_URL = `${host}${path}`;
 
 // TODO
 //import EXPECTED_DID_DOC from './expected-did-doc.json' assert {type: 'json'};
 import {expectedDidDoc as EXPECTED_DID_DOC} from './expected-data.js';
+nock(host).get(path).reply(200, EXPECTED_DID_DOC);
 
 describe('did:web method driver', () => {
   describe('get', () => {
