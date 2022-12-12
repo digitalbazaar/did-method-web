1843e645445a1f7b87ffbaa0bae7b48d4f19b59d
Andrew Jones <ajones@digitalbazaar.com>
2022-12-12 17:42:30 -0500

    Correct 2018 verificationMethod test.

--- test/expected-data.js
+++ test/expected-data.js
@@ -38,4 +38,47 @@ export const expectedDidDoc = {
     }
   ]
 };
+
+export const expectedDidDoc2018 = {
+  '@context': [
+    'https://www.w3.org/ns/did/v1',
+    'https://w3id.org/security/suites/ed25519-2018/v1',
+    'https://w3id.org/security/suites/x25519-2019/v1'
+  ],
+  id: 'did:web:w3c-ccg.github.io:user:alice',
+  verificationMethod: [
+    {
+      id: 'did:web:w3c-ccg.github.io:user:alice' +
+        '#z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH',
+      type: 'Ed25519VerificationKey2018',
+      controller: 'did:web:w3c-ccg.github.io:user:alice',
+      publicKeyBase58: 'B12NYF8RrR3h41TDCTJojY59usg3mbtbjnFs7Eud1Y6u'
+    }
+  ],
+  authentication: [
+    'did:web:w3c-ccg.github.io:user:alice' +
+    '#z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH'
+  ],
+  assertionMethod: [
+    'did:web:w3c-ccg.github.io:user:alice' +
+      '#z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH'
+  ],
+  capabilityDelegation: [
+    'did:web:w3c-ccg.github.io:user:alice' +
+      '#z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH'
+  ],
+  capabilityInvocation: [
+    'did:web:w3c-ccg.github.io:user:alice' +
+      '#z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH'
+  ],
+  keyAgreement: [
+    {
+      id: 'did:web:w3c-ccg.github.io:user:alice' +
+          '#z6LSbysY2xFMRpGMhb7tFTLMpeuPRaqaWM1yECx2AtzE3KCc',
+      type: 'X25519KeyAgreementKey2019',
+      controller: 'did:web:w3c-ccg.github.io:user:alice',
+      publicKeyBase58: 'JhNWeSVLMYccCk7iopQW4guaSJTojqpMEELgSLhKwRr'
+    }
+  ]
+};
 /* eslint-enable */
