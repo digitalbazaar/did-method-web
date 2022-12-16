8dc02cc6d98e8f2e9aa38363965b9681fd5df31f
Andrew Jones <ajones@digitalbazaar.com>
2022-12-16 16:54:08 -0500

    Move test data to constants & add initial vc suite.

---
+++ test/constants.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ */
+
+export const TEST_SEED = '8c2114a150a16209c653817acc7f3e7e9c6c6290ae93d6689' +
+  'cbd61bb038cd31b';
+export const TEST_DID = 'did:web:w3c-ccg.github.io:user:alice';
+const host = 'https://w3c-ccg.github.io';
+const path = '/user/alice';
+export const TEST_URL = `${host}${path}`;
+export const FILE_URL = `${TEST_URL}/did.json`;
---
+++ test/vc.spec.js
@@ -0,0 +1,19 @@
+/*!
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as vc from '@digitalbazaar/vc';
+import chai from 'chai';
+import {driver} from '../lib/index.js';
+
+chai.should();
+const {expect} = chai;
+
+describe('vc', function() {
+  describe('Sign', function() {
+
+  });
+  describe('Verify', function() {
+
+  });
+});
+
