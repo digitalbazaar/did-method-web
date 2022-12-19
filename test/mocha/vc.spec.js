0bed9cb03cde074ffc4908d1c41f7fa22052c6eb
Andrew Jones <ajones@digitalbazaar.com>
2022-12-19 12:08:02 -0500

    Add working tests for vc issue and verify.

--- test/mocha/vc.spec.js
+++ test/mocha/vc.spec.js
@@ -8,6 +8,7 @@ import {
   TEST_URL,
 } from '../constants.js';
 import chai from 'chai';
+import {documentLoader} from '../documentLoader.js';
 import {driver} from '../../lib/index.js';
 import {Ed25519Signature2020} from '@digitalbazaar/ed25519-signature-2020';
 
@@ -20,32 +21,47 @@ const credential = {
   ],
   id: 'https://example.com/credentials/1872',
   type: ['VerifiableCredential'],
-  issuer: 'https://example.edu/issuers/565049',
+  issuer: TEST_DID,
   issuanceDate: '2010-01-01T19:23:24Z',
-  credentialSubject: {id: 'urn:test:issue-did-web'}
+  credentialSubject: {id: TEST_DID}
 };
 
-describe.only('vc', function() {
+describe('vc', function() {
+  let key;
   let suite;
   before(async function() {
-    console.log('vc before');
     const seedBytes = (new TextEncoder()).encode(TEST_SEED).slice(0, 32);
     const {methodFor} = await didWebDriver.generate({
       seed: seedBytes,
       url: TEST_URL
     });
-    const key = methodFor({purpose: 'assertionMethod'});
+    key = methodFor({purpose: 'assertionMethod'});
     suite = new Ed25519Signature2020({key});
   });
   describe('Sign', async function() {
     it('should issue a Vc', async function() {
-      const issuedVc = await vc.issue({suite, credential});
+      const issuedVc = await vc.issue({suite, credential, documentLoader});
       expect(issuedVc).to.exist;
+      expect(issuedVc).to.be.an('object');
+      expect(issuedVc.proof).to.exist;
+      expect(issuedVc.proof).to.be.an('object');
+      expect(issuedVc.proof.verificationMethod).to.exist;
+      expect(issuedVc.proof.verificationMethod).to.be.a('string');
+      expect(issuedVc.proof.verificationMethod).to.equal(key.id);
     });
   });
   describe('Verify', function() {
     it('should verify a vc', async function() {
-      const suite = new Ed25519Signature2020();
+      const issuedVc = await vc.issue({suite, credential, documentLoader});
+      expect(issuedVc).to.exist;
+      const verifyResult = await vc.verifyCredential({
+        suite: new Ed25519Signature2020(),
+        credential,
+        documentLoader
+      });
+      expect(verifyResult).to.exist;
+      expect(verifyResult.verified).to.exist;
+      expect(verifyResult.verified).to.equal(true);
     });
   });
 });
