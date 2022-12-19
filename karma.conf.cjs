1149bf262c5fe35197de135bc278da1a8e65ab7f
Andrew Jones <ajones@digitalbazaar.com>
2022-12-19 12:30:22 -0500

    Update karma config to run tests and preprocessors on mocha dir.

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/*.spec.js'
+      'test/mocha/*.spec.js'
     ],
 
     // list of files to exclude
@@ -22,7 +22,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.js': ['webpack', 'sourcemap']
+      'test/**/*.js': ['webpack', 'sourcemap']
     },
 
     webpack: {
